c0d29150b32da896b9c6dff0be33456abd3eb585
luismarymo <luchita0420@gmail.com>
2023-11-06 12:55:26 +0100

    W09 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 5);
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("Grey", 5, 2);
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("Pink", 2);
+        // Console.WriteLine(circle.GetColor());
+        // Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>()
+        {
+            square, rectangle, circle
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}");
+        }
     }
 }
